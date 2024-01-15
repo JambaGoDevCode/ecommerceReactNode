14dd52b98da7aacd8d992d3b883ad98bfdf7405c
João Jamba <jambapedrojoao@gmail.com>
2024-01-15 19:47:31 +0100

    feat: implemente de navigation sistem

--- ecomerce/src/App.jsx
+++ ecomerce/src/App.jsx
@@ -1,12 +1,20 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+import Login from './pages/auth/Login'
+import Register from './pages/auth/Register'
+import Home from './pages/Home'
 
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      <p>React aplication</p>
-    </>
+    <Routes> 
+      <Route path='/' Component={Home}/>
+      <Route path='/login' Component={Login}/>
+      <Route path='/register' Component={Register}/>
+    </Routes>
   )
 }
 
